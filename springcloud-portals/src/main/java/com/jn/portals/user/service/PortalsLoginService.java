eea8543bac6e4086b502866283d49f44b90f2223
yangph <yangph@op-mobile.com.cn>
2018-12-04 17:27:50 +0800

    task:5331 登录接口修改，自测，联调
    Change-Id: I169568dcb10a7d879273ca00da3bde0f6b51d0ad

---
+++ springcloud-portals/src/main/java/com/jn/portals/user/service/PortalsLoginService.java
@@ -0,0 +1,23 @@
+package com.jn.portals.user.service;
+
+
+import com.jn.portals.model.PortalsUser;
+
+import java.util.List;
+
+
+/**
+ * 平台登录
+ * @Author: yangph
+ * @Date: 2018/11/28 9:39
+ * @Version v1.0
+ * @modified By:
+ */
+public interface PortalsLoginService {
+    /**
+     * 根据用户账号获取用户的个人拓展信息和企业拓展信息
+     * @param account 用户账号
+     * @return
+     */
+    List<PortalsUser> getUserOutreachInfo(String account);
+}

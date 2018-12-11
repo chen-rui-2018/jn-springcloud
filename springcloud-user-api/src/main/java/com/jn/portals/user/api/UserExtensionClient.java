9bd35a0d85071ae47b03df56328e1f2b4efc6efe
yangph <yangph@op-mobile.com.cn>
2018-12-11 09:32:22 +0800

    task:5331 登录接口修改，自测，联调   删除登录代码，完善获取用户扩展信息
    Change-Id: I28e3bc73ecf3c37b826be698f50c6910d692ffb2

--- springcloud-user-api/src/main/java/com/jn/portals/user/api/UserExtensionClient.java
+++ springcloud-user-api/src/main/java/com/jn/portals/user/api/UserExtensionClient.java
@@ -1,7 +1,7 @@
 package com.jn.portals.user.api;
 
 import com.jn.common.model.Result;
-import com.jn.portals.user.model.PortalsUser;
+import com.jn.portals.user.model.UserExtension;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @Version v1.0
  * @modified By:
  */
-@FeignClient("springcloud-portals")
-public interface PortalLoginClient {
+@FeignClient("springcloud-user")
+public interface UserExtensionClient {
     /**
      * 获取登录用户扩展信息
      *
@@ -24,5 +24,5 @@ public interface PortalLoginClient {
      * @return
      */
     @RequestMapping(value = "/api/portals/getUserExtension", method = RequestMethod.POST)
-    Result<PortalsUser> getUserExtension(@RequestBody String account);
+    Result<UserExtension> getUserExtension(@RequestBody String account);
 }
---
+++ springcloud-user/src/main/java/com/jn/portals/user/controller/UserInfoController.java
@@ -0,0 +1,11 @@
+package com.jn.portals.user.controller;
+
+/**
+ * 用户信息
+ * @Author: yangph
+ * @Date: 2018/12/10 11:29
+ * @Version v1.0
+ * @modified By:
+ */
+public class UserInfoController {
+}

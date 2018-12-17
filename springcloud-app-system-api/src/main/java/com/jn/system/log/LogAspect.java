67babefa752c5d4d52d23318bd8fa59913c9a5d6
申佩衡 <shenph@op-mobile.com.cn>
2018-12-17 21:07:46 +0800

    增加业务错误日志

--- springcloud-app-system-api/src/main/java/com/jn/system/log/LogAspect.java
+++ springcloud-app-system-api/src/main/java/com/jn/system/log/LogAspect.java
@@ -30,50 +30,45 @@ public class LogAspect {
     private static Logger logger = LoggerFactory.getLogger(LogAspect.class);
 
 
-
     @Around(value = " @annotation(log)", argNames = "joinPoint, log")
     public Object methodAround(ProceedingJoinPoint joinPoint, ControllerLog log) throws Throwable {
         String accountName = this.getUserName();
-        logger.info("begin.用户【{}】，进入【{}】的业务，对应的应用地址：【{}】",accountName,log.doAction(),joinPoint.toString());
+        logger.info("begin.用户【{}】，进入【{}】的业务，对应的应用地址：【{}】", accountName, log.doAction(), joinPoint.toString());
         Object[] args = joinPoint.getArgs();
-        if (args != null){
-            for(int i = 0 ;i < args.length; i++) {
-                logger.info("入参{}：【{}】",i,args[i]+"");
+        if (args != null) {
+            for (int i = 0; i < args.length; i++) {
+                logger.info("入参{}：【{}】", i, args[i] + "");
             }
         }
         long start = System.currentTimeMillis();
         Object result = null;
         try {
             result = joinPoint.proceed();
-        }catch (Exception e){
+        } catch (Exception e) {
+            logger.warn("用户【{}】，执行【{}】的业务，错误信息【{}】", accountName, log.doAction(), e.toString());
             throw e;
-        }finally {
+        } finally {
             long end = System.currentTimeMillis();
-            logger.info("end.用户【{}】，完成【{}】的业务，耗时：【{}】,结果是：【{}】",accountName,log.doAction(),(end-start)+"ms",result+"");
+            logger.info("end.用户【{}】，完成【{}】的业务，耗时：【{}】,结果是：【{}】", accountName, log.doAction(), (end - start) + "ms", result + "");
         }
         return result;
     }
 
     @Before(value = "@annotation(log)", argNames = "joinPoint, log")
-    public void methodBefore(JoinPoint joinPoint, ServiceLog log){
-        logger.info("进入【{}】的方法，对应的应用地址：【{}】",log.doAction(),joinPoint.toString());
+    public void methodBefore(JoinPoint joinPoint, ServiceLog log) {
+        logger.info("进入【{}】的方法，对应的应用地址：【{}】", log.doAction(), joinPoint.toString());
     }
 
 
     /**
      * 获取用户账号
+     *
      * @return
      */
-    private String getUserName(){
+    private String getUserName() {
         User user = (User) SecurityUtils.getSubject().getPrincipal();
-        return user==null?"guest":user.getAccount();
+        return user == null ? "guest" : user.getAccount();
     }
-
-
-
-
-
-
 
 
 }

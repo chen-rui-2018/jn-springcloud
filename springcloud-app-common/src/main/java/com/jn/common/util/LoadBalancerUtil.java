f6e4edfe91098c357cc749e80ea6eb7469215240
申佩衡 <shenph@op-mobile.com.cn>
2019-01-21 18:02:14 +0800

    RestTemplateUtil 工具类

--- springcloud-app-common/src/main/java/com/jn/common/util/LoadBalancerUtil.java
+++ springcloud-app-common/src/main/java/com/jn/common/util/LoadBalancerUtil.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -31,7 +29,7 @@ public class LoadBalancerUtil {
     /**
      * RestTemplate 调用接口
      *
-     * @param sysId     请求spring.application.name
+     * @param sysId      请求spring.application.name
      * @param url        请求地址
      * @param jsonObject 请求参数
      * @return
@@ -40,23 +38,20 @@ public class LoadBalancerUtil {
         ServiceInstance si = this.loadBalancerClient.choose(sysId);
         StringBuffer sb = new StringBuffer();
         sb.append("http://").append(si.getHost()).append(":").append(si.getPort()).append(url);
-        HttpHeaders headers = new HttpHeaders();
-        MediaType type = MediaType.parseMediaType(MediaType.APPLICATION_JSON_UTF8_VALUE);
-        headers.setContentType(type);
-        headers.add(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON.toString());
-        HttpEntity<String> formEntity = new HttpEntity<>(jsonObject, headers);
+        HttpEntity<String> formEntity = new HttpEntity<>(jsonObject, RestTemplateUtil.setHeaders());
         ResponseEntity<Result> response = restTemplate.postForEntity(sb.toString(), formEntity, Result.class);
         return response.getBody();
     }
 
     /**
      * 判断客户端是否有启动
+     *
      * @param sysId
      * @return
      */
     public Boolean isClientUp(String sysId) {
         ServiceInstance si = this.loadBalancerClient.choose(sysId);
-        if(si == null){
+        if (si == null) {
             return Boolean.FALSE;
         }
         return Boolean.TRUE;

5dc6676bd583dfd9cbd27c9a853c3d2b3086d96d
chenm <chenmiao@op-mobile.com.cn>
2019-03-12 15:42:30 +0800

    小程序授权登陆
    Change-Id: Ie19ea89639881216cb318461461cf54b13f277c0

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -2,7 +2,37 @@ const openIdUrl = require('./config').openIdUrl
 
 App({
   onLaunch(opts) {
-    console.log('App Launch', opts)
+    console.log('App Launch', opts);
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+              console.log(111111111);
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        } else {
+          wx.reLaunch({
+            url: '/page/minibase/authorize/authorize',
+          })
+        }
+      }
+    })
   },
   onShow(opts) {
     console.log('App Show', opts)
@@ -12,7 +42,8 @@ App({
   },
   globalData: {
     hasLogin: false,
-    openid: null
+    openid: null,
+    urlPath: 'http://sw8cyn.natappfree.cc/springcloud-wechat-miniprogram/'
   },
   // lazy loading openid
   getUserOpenId(callback) {

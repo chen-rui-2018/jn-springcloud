5870154803ae4108eceeacbc5f4057ce0a76babb
shenpeiheng <>
2019-03-15 17:41:46 +0800

    小程序 我的页面编写

--- miniprogram/page/minibase/user/index.js
+++ miniprogram/page/minibase/user/index.js
@@ -1,4 +1,4 @@
-// const app = getApp()
+const app = getApp()
 
 Page({
   data: {
@@ -6,30 +6,30 @@ Page({
     items: [
       {
         icon: '../../../image/my/iconfont-order.png',
-        text: '我的订单',
+        text: '我的爱车',
         path: '/pages/order/list/index'
       },
       {
         icon: '../../../image/my/iconfont-addr.png',
-        text: '收货地址',
+        text: '我的报修',
         path: '/pages/address/list/index'
       },
       {
         icon: '../../../image/my/iconfont-kefu.png',
-        text: '联系客服',
-        path: '18521708248',
+        text: '我的活动',
+        path: '/pages/address/list/index',
       },
       {
         icon: '../../../image/my/iconfont-help.png',
-        text: '常见问题',
+        text: '缴费记录',
         path: '/pages/help/list/index',
       },
     ],
     settings: [
       {
         icon: '../../../image/my/iconfont-clear.png',
-        text: '清除缓存',
-        path: '0.0KB'
+        text: '修改密码',
+        path: '/pages/address/list/index',
       },
       {
         icon: '../../../image/my/iconfont-about.png',
@@ -39,8 +39,32 @@ Page({
     ]
   },
   onLoad() {
-    this.getUserInfo()
-    // this.getStorageInfo()
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
   navigateTo(e) {
     const index = e.currentTarget.dataset.index
@@ -56,7 +80,13 @@ Page({
         App.WxService.navigateTo(path)
     }
   },
-  getUserInfo() {
+  getUserInfo(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   },
   getStorageInfo() {
   },

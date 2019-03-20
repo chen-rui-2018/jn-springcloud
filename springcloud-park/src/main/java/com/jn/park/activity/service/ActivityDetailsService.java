322c0bb4f915f5769a9b5d6a63ebf47695fe66a3
yangph <yangph@op-mobile.com.cn>
2019-03-20 11:07:38 +0800

    task 5978 活动模块-【活动管理】联调测试、修改-改版维护 修改评论详情，添加判断当前用是否已点赞评论的字段，修改判断逻辑，修改测试类
    Change-Id: I88e076d3a07db91147089f08a15f6d1bb9b05123

--- springcloud-park/src/main/java/com/jn/park/activity/service/ActivityDetailsService.java
+++ springcloud-park/src/main/java/com/jn/park/activity/service/ActivityDetailsService.java
@@ -31,9 +31,10 @@ public interface ActivityDetailsService {
      * 根据活动id获取活动点评信息
      * @param activityQueryPaging
      * @param isPage     是否分页  true：分页   false:不分页
+     * @param loginAccount 当前登录用来账号
      * @return
      */
-    PaginationData getCommentInfo(ActivityQueryPaging activityQueryPaging, Boolean isPage);
+    PaginationData getCommentInfo(ActivityQueryPaging activityQueryPaging,String loginAccount, Boolean isPage);
 
     /**
      * 根据活动id获取活动点赞信息

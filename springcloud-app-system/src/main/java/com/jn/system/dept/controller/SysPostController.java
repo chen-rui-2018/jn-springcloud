027bb27fe3308d42c4d3f5fcd79070607e6bc02f
shenpeiheng <>
2019-03-22 11:27:44 +0800

    swagger...

--- springcloud-app-system/src/main/java/com/jn/system/dept/controller/SysPostController.java
+++ springcloud-app-system/src/main/java/com/jn/system/dept/controller/SysPostController.java
@@ -12,6 +12,7 @@ import com.jn.system.dept.service.SysPostService;
 import com.jn.system.log.annotation.ControllerLog;
 import com.jn.system.model.User;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -20,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
@@ -44,17 +46,17 @@ public class SysPostController extends BaseController {
 
     @ControllerLog(doAction = "查询所有岗位")
     @RequiresPermissions("/system/sysPost/findSysPostAll")
-    @ApiOperation(value = "查询所有岗位", httpMethod = "POST", response = Result.class)
-    @RequestMapping(value = "/findSysPostAll")
-    public Result findSysPostAll() {
+    @ApiOperation(value = "查询所有岗位", notes = "查询所有岗位")
+    @RequestMapping(value = "/findSysPostAll",method = RequestMethod.POST)
+    public Result<List<TbSysPost>> findSysPostAll() {
         List<TbSysPost> sysPostAll = sysPostService.findSysPostAll();
         return new Result(sysPostAll);
     }
 
     @ControllerLog(doAction = "添加岗位")
     @RequiresPermissions("/system/sysPost/add")
-    @ApiOperation(value = "添加岗位", httpMethod = "POST", response = Result.class)
-    @RequestMapping(value = "/add")
+    @ApiOperation(value = "添加岗位", notes = "添加岗位")
+    @RequestMapping(value = "/add",method = RequestMethod.POST)
     public Result add(@Validated @RequestBody SysPostAdd sysPostAdd) {
         //获取当前登录用户信息
         User user = (User) SecurityUtils.getSubject().getPrincipal();
@@ -70,8 +72,8 @@ public class SysPostController extends BaseController {
 
     @ControllerLog(doAction = "逻辑删除岗位信息")
     @RequiresPermissions("/system/sysPost/delete")
-    @ApiOperation(value = "逻辑删除岗位信息", httpMethod = "POST", response = Result.class)
-    @RequestMapping(value = "/delete")
+    @ApiOperation(value = "逻辑删除岗位信息", notes = "逻辑删除岗位信息")
+    @RequestMapping(value = "/delete",method = RequestMethod.POST)
     public Result delete(String[] postIds) {
         Assert.noNullElements(postIds, "岗位id不能为空");
         //获取当前登录用户信息
@@ -82,8 +84,8 @@ public class SysPostController extends BaseController {
 
     @ControllerLog(doAction = "修改岗位信息")
     @RequiresPermissions("/system/sysPost/update")
-    @ApiOperation(value = "修改岗位信息", httpMethod = "POST", response = Result.class)
-    @RequestMapping(value = "/update")
+    @ApiOperation(value = "修改岗位信息", notes = "修改岗位信息")
+    @RequestMapping(value = "/update",method = RequestMethod.POST)
     public Result update(@Validated @RequestBody SysPost sysPost) {
         Assert.notNull(sysPost.getId(), "岗位id不能为空");
         //获取当前登录用户信息
@@ -94,27 +96,27 @@ public class SysPostController extends BaseController {
 
     @ControllerLog(doAction = "根据岗位id获取岗位信息")
     @RequiresPermissions("/system/sysPost/selectByPrimaryKey")
-    @ApiOperation(value = "根据岗位id获取岗位信息", httpMethod = "POST", response = Result.class)
-    @RequestMapping(value = "/selectByPrimaryKey")
-    public Result selectByPrimaryKey(String id) {
+    @ApiOperation(value = "根据岗位id获取岗位信息", notes = "根据岗位id获取岗位信息")
+    @RequestMapping(value = "/selectByPrimaryKey",method = RequestMethod.POST)
+    public Result<SysPost> selectByPrimaryKey(String id) {
         SysPost sysPost = sysPostService.selectByPrimaryKey(id);
         return new Result(sysPost);
     }
 
     @ControllerLog(doAction = "条件分页获取岗位信息列表")
     @RequiresPermissions("/system/sysPost/list")
-    @ApiOperation(value = "条件分页获取岗位信息列表", httpMethod = "POST", response = Result.class)
-    @RequestMapping(value = "/list")
-    public Result list(@Validated @RequestBody SysPostPage sysPostPage) {
+    @ApiOperation(value = "条件分页获取岗位信息列表", notes = "条件分页获取岗位信息列表")
+    @RequestMapping(value = "/list",method = RequestMethod.POST)
+    public Result<PaginationData<List<SysPost>>> list(@Validated @RequestBody SysPostPage sysPostPage) {
         PaginationData data = sysPostService.findByPage(sysPostPage);
         return new Result(data);
     }
 
-    @ControllerLog(doAction = "校验岗位名称是否存在,fail表示名称已存在,success表示可以使用")
-    @ApiOperation(value = "校验岗位名称是否存在,fail表示名称已存在,success表示可以使用", httpMethod = "POST", response = Result.class)
-    @RequestMapping(value = "/checkPostName")
+    @ControllerLog(doAction = "校验岗位名称是否存在")
+    @ApiOperation(value = "校验岗位名称是否存在,", notes = "fail表示名称已存在,success表示可以使用")
+    @RequestMapping(value = "/checkPostName",method = RequestMethod.POST)
     @RequiresPermissions("/system/sysPost/checkPostName")
-    public Result checkPostName(String postName) {
+    public Result<String> checkPostName(String postName) {
         String result = sysPostService.checkPostName(postName);
         return new Result(result);
     }

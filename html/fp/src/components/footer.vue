7db1228e1812a1267da78055943b008f1143509e
zhouj <zhoujing@op-mobile.com.cn>
2019-03-27 10:18:38 +0800

    task：7773 门户登录、注册页面开发
    Change-Id: Ibab404c2b6a63fd3adf13069f3da0da48503735a

---
+++ html/fp/src/components/footer.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="footer">
+    <div class="container">
+      <div class="footer1 clearfix">
+        <div class="footLeft">
+          <div class="help">
+            <h5>门户</h5>
+            <div class="line"></div>
+            <ul>
+              <li>
+                <a href="javascript:viod(0)">首页</a>
+              </li>
+              <li>
+                <a href="javascript:viod(0)">招商引资</a>
+              </li>
+              <li>
+                <a href="javascript:viod(0)">智慧党建</a>
+              </li>
+              <li>
+                <a href="javascript:viod(0)">企业服务</a>
+              </li>
+            </ul>
+          </div>
+          <div class="help">
+            <h5>帮助</h5>
+            <div class="line"></div>
+            <ul>
+              <li>
+                <a href="javascript:viod(0)">联系我们</a>
+              </li>
+              <li>
+                <a href="javascript:viod(0)">关于我们</a>
+              </li>
+              <li>
+                <a href="javascript:viod(0)">申请企业账户</a>
+              </li>
+              <li>
+                <a href="javascript:viod(0)">申请顾问</a>
+              </li>
+            </ul>
+          </div>
+          <div class="attention">
+            <h5>关注我们</h5>
+            <div class="line"></div>
+            <div class="attentionIcon">
+              <i class="iconfont icon-12sina"></i>
+              <i class="iconfont icon-weixin"></i>
+            </div>
+          </div>
+          <div class="erweima clearfix">
+            <img src="@/../static/img/weixin.png" alt="" class="fl">
+             <!-- <i class="iconfont icon-qr-code"></i> -->
+            <div class="aboutUs fr">
+              <i class="iconfont icon-weixin1"></i>
+              <p>扫描二维码</p>
+              <p>关注我们</p>
+            </div>
+          </div>
+        </div>
+        <div class="footRight">
+          <div class="right1 arialn">HOTLINE</div>
+          <div class="phone impact">400-800-9888</div>
+          <div class="problem">您有任何问题，都可以联系我们</div>
+          <div class="book">
+            <a>立即订阅，获得75折优惠</a>
+            <i class="iconfont icon-toright"></i>
+          </div>
+        </div>
+      </div>
+      <div class="footer2">
+        京ICP备09046754&copy;版权所有
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.icon-weixin{
+  font-size: 18px;
+}
+.icon-weixin1{
+  font-size: 20px;
+}
+.footer {
+  border-top:1px solid #ccc;
+  .container {
+    .footer1 {
+      height: 240px;
+      width: 1190px;
+      margin: 0 auto;
+      .footLeft {
+        margin-top: 50px;
+        float: left;
+        .help {
+          margin-right: 30px;
+          display: inline-block;
+          h5 {
+            font-size: 14px;
+          }
+          ul {
+            li {
+              margin-bottom: 5px;
+              a {
+                font-size: 12px;
+                color: rgba(112, 112, 112, 0.5);
+              }
+            }
+          }
+        }
+        .attention {
+          display: inline-block;
+          vertical-align: top;
+          >.attentionIcon{
+            margin-top:8px;
+            color: #999;
+          }
+        }
+        .erweima {
+          padding:5px;
+          display: inline-block;
+          vertical-align: top;
+          border: 1px solid #eee;
+          height: 100px;
+          width: 200px;
+          margin-left: 30px;
+          text-align: center;
+          >img{
+            width: 100px;
+            height: 100px;
+          }
+          >.aboutUs{
+            margin-top:20px;
+            margin-right:10px;
+            float:right;
+            line-height: 20px;
+            color: #999;
+            >p{
+              font-size: 12px;
+            }
+          }
+        }
+        .line {
+            height: 1px;
+            width: 12px;
+            background: #000;
+            font-weight: bold;
+            margin: 10px 0 15px 0;
+          }
+      }
+      .footRight {
+        float: right;
+        text-align: right;
+        .right1 {
+          color: #707070;
+          font-size: 14px;
+          margin-top: 40px;
+        }
+        .phone {
+          color: #00a041;
+          font-size: 47px;
+        }
+        .problem {
+          font-size: 12px;
+          color: #9e9e9e;
+        }
+        .book {
+          height: 38px;
+          line-height: 38px;
+          padding: 0 10px;
+          width: 243px;
+          color: rgba(112, 112, 112, 0.5);
+          font-size: 12px;
+          border: 1px solid rgba(112, 112, 112, 0.5);
+          margin-top: 20px;
+          margin-left: 70px;
+          a {
+            float: left;
+          }
+        }
+      }
+    }
+    .footer2 {
+      color: #fff;
+      height: 48px;
+      line-height: 48px;
+      background-color: #000;
+      text-align: left;
+      padding-left: 150px;
+    }
+  }
+}
+</style>
---
+++ html/fp/src/views/login/index.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="loginbg">
+    <div class="loginLogo">
+      <img src="@/../static/img/login-logo.png" alt="">
+    </div>
+    <div class="loginBox">
+      <el-form ref="loginform" :model="loginform">
+          <input type="text" placeholder="请输入手机号码" v-model.trim="loginform.account">
+          <input type="password" placeholder="请输入密码" v-model.trim="loginform.password">
+        <span class="forgetPass" @click="handleForpsw">忘记密码？</span>
+          <el-button plain style="width:100%;height:40px;line-height:14px;border:1px solid #41d787;color:#00a041;background:#ecfcf2;font-size:14px;" @click="loginForm('loginform')">登&nbsp;录</el-button>
+        <span class="register" @click="handleRester">立即注册</span>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginform: {
+        account: "",
+        password: ""
+      },
+      input: ""
+    };
+  },
+  created() {
+    // this.init();
+  },
+  methods: {
+    handleForpsw() {
+      this.$router.push({ path: "forgetPsw" });
+    },
+    handleRester(){
+      this.$router.push({ path: 'register' })
+    },
+    loginForm(loginform) {
+      if(!this.loginform.account){
+        this.$message.error('请输入用户名')
+        return
+      }
+      if(!this.loginform.password){
+        this.$message.error('请输入密码')
+        return
+      }
+      let _this = this;
+      this.api.post({
+        url: "loginURL",
+        data: {
+          account: _this.loginform.account,
+          password: _this.loginform.password
+        },
+        dataFlag: false,
+        callback: function(res) {
+          if (res.code == "0000") {
+            sessionStorage.token = res.data;
+            _this.api.setToken(res.data)
+            _this.$router.push({ path: "/user/userHome", query: { account: _this.loginform.account } })
+          } else{
+            _this.$message.error(res.result)
+          }
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.loginbg {
+  font-family: "微软雅黑";
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: -1;
+  background: url("../../../static/img/login-bg.png") 100% 100% / 100% 100%
+    no-repeat;
+    .loginLogo{
+      width: 155px;
+      height: 38px;
+      img{
+        width: 100%;
+        height: 100%;
+        padding-left:160px;
+        padding-top:35px;
+      }
+    }
+  .loginBox {
+    background: #fff;
+    padding: 45px 75px;
+    width: 266px;
+    height: 185px;
+    // height: 220px;
+    border-radius: 10px;
+    position: relative;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    input {
+      height: 40px;
+      border: 1px solid #eee;
+      width: 266px;
+      color: #333;
+      font-size: 13px;
+      text-indent: 15px;
+      border-radius: 5px;
+    }
+    input:nth-child(1){
+      margin-bottom:20px;
+    }
+    input::-webkit-input-placeholder {
+      color: #b7b7b7;
+      font-size: 12px;
+    }
+    // .el-form-item:nth-child(2) {
+    //   margin-bottom: 0;
+    // }
+    .forgetPass {
+      display: inline-block;
+      font-size: 13px;
+      color: #f00;
+      margin: 5px 0;
+      margin-left: 16px;
+      margin-bottom: 10px;
+      cursor: pointer;
+    }
+    .register {
+      display: inline-block;
+      font-size: 13px;
+      color: #f00;
+      position: absolute;
+      right: 20px;
+      bottom: 10px;
+      cursor: pointer;
+    }
+  }
+}
+</style>

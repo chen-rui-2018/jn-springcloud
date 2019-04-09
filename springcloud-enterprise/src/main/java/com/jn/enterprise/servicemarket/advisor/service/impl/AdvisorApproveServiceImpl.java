75134faa2d3b5b1e020238f4d0b0ace6e00b3937
yangph <yangph@op-mobile.com.cn>
2019-04-09 15:27:30 +0800

    task:8118 用户中心-角色认证
    Change-Id: I1644b7cdbd0a1f1b692d94613e5edeaa82994ef2

---
+++ springcloud-enterprise/src/main/java/com/jn/enterprise/servicemarket/advisor/service/impl/AdvisorApproveServiceImpl.java
@@ -0,0 +1,61 @@
+package com.jn.enterprise.servicemarket.advisor.service.impl;
+
+import com.jn.enterprise.servicemarket.advisor.controller.AdvisorApproveController;
+import com.jn.enterprise.servicemarket.advisor.model.OrgInfoShow;
+import com.jn.enterprise.servicemarket.advisor.service.AdvisorApproveService;
+import com.jn.enterprise.servicemarket.org.dao.TbServiceOrgMapper;
+import com.jn.enterprise.servicemarket.org.entity.TbServiceOrg;
+import com.jn.enterprise.servicemarket.org.entity.TbServiceOrgCriteria;
+import com.jn.system.log.annotation.ServiceLog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: yangph
+ * @Date: 2019/4/9 9:52
+ * @Version v1.0
+ * @modified By:
+ */
+@Service
+public class AdvisorApproveServiceImpl implements AdvisorApproveService {
+    /**
+     * 日志组件
+     */
+    private static Logger logger = LoggerFactory.getLogger(AdvisorApproveController.class);
+    /**
+     * 是否删除  0：已删除   1：有效
+     */
+    private static final byte RECORD_STATUS=1;
+
+    @Autowired
+    private TbServiceOrgMapper tbServiceOrgMapper;
+
+    /**
+     * 选择机构
+     * @param orgName 机构名称
+     * @return
+     */
+    @ServiceLog(doAction = "选择机构")
+    @Override
+    public List<OrgInfoShow> selectOrgInfo(String orgName) {
+        TbServiceOrgCriteria example=new TbServiceOrgCriteria();
+        example.createCriteria().andOrgStatusEqualTo("1").andRecordStatusEqualTo(RECORD_STATUS);
+        List<TbServiceOrg> tbServiceOrgList = tbServiceOrgMapper.selectByExample(example);
+        if(tbServiceOrgList.isEmpty()){
+            return null;
+        }
+        List<OrgInfoShow> resultList=new ArrayList<>(16);
+        for(TbServiceOrg tbServiceOrg:tbServiceOrgList){
+            OrgInfoShow orgInfoShow=new OrgInfoShow();
+            orgInfoShow.setOrgId(tbServiceOrg.getOrgId());
+            orgInfoShow.setOrgName(tbServiceOrg.getOrgName());
+            resultList.add(orgInfoShow);
+        }
+        return resultList;
+    }
+}

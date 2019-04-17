aa4fe160a0fb9d4949a284d670f5ab6487093140
chenqiong <chenqiong666666@163.com>
2019-04-17 10:49:30 +0800

    Merge remote-tracking branch 'origin/develop-nanjing' into develop-jn
    Change-Id: I3d7cef67b82a70d33be537acb1fd3c46da260e36

--- html/cms/src/router/modules/portal.js
+++ html/cms/src/router/modules/portal.js
@@ -49,67 +49,67 @@ const portalRouter = {
       name: 'applyActivityList',
       meta: { title: '活动报名列表', noCache: false },
       hidden: true
-    },
-    {
-      path: 'ServiceManagement',
-      component: () => import('@/views/portal/ServiceManagement'),
-      name: 'ServiceManagement',
-      meta: { title: '服务管理', noCache: true },
-      children: [
-        {
-          path: 'featuredProduct',
-          component: () => import('@/views/portal/ServiceManagement/featuredProduct'),
-          name: 'featuredProduct',
-          meta: { title: '特色服务产品', noCache: true }
-        },
-        {
-          path: 'productApprover/:id',
-          component: () => import('@/views/portal/ServiceManagement/productApprover'),
-          name: 'productApprover',
-          meta: { title: '特色服务产品审批非科技金融', noCache: false },
-          hidden: true
-        },
-        {
-          path: 'productApproverFinance/:id',
-          component: () => import('@/views/portal/ServiceManagement/productApproverFinance'),
-          name: 'productApproverFinance',
-          meta: { title: '特色服务产品审批科技金融', noCache: false }
-
-        },
-        {
-          path: 'findServiceDetail/:id',
-          component: () => import('@/views/portal/ServiceManagement/findServiceDetail'),
-          name: 'findServiceDetail',
-          meta: { title: '特色服务产品查看非科技金融', noCache: false },
-          hidden: true
-        },
-        {
-          path: 'findServiceDetailfinance/:id',
-          component: () => import('@/views/portal/ServiceManagement/findServiceDetailfinance'),
-          name: 'findServiceDetailfinance',
-          meta: { title: '特色服务产品查看科技金融', noCache: false },
-          hidden: true
-        },
-        {
-          path: 'generalProduct',
-          component: () => import('@/views/portal/ServiceManagement/generalProduct'),
-          name: 'generalProduct',
-          meta: { title: '服务产品管理', noCache: true }
-        }
-      ]
-    },
-    {
-      path: 'requireManagement',
-      component: () => import('@/views/portal/requireManagement'),
-      name: 'requireManagement',
-      meta: { title: '需求管理', noCache: true }
-    },
-    {
-      path: 'requireDetail',
-      component: () => import('@/views/portal/requireDetail'),
-      name: 'requireDetail',
-      meta: { title: '查看详情', noCache: true }
     }
+    // {
+    //   path: 'ServiceManagement',
+    //   component: () => import('@/views/portal/ServiceManagement'),
+    //   name: 'ServiceManagement',
+    //   meta: { title: '服务管理', noCache: true },
+    //   children: [
+    //     {
+    //       path: 'featuredProduct',
+    //       component: () => import('@/views/portal/ServiceManagement/featuredProduct'),
+    //       name: 'featuredProduct',
+    //       meta: { title: '特色服务产品', noCache: true }
+    //     },
+    //     {
+    //       path: 'productApprover/:id',
+    //       component: () => import('@/views/portal/ServiceManagement/productApprover'),
+    //       name: 'productApprover',
+    //       meta: { title: '特色服务产品审批非科技金融', noCache: false },
+    //       hidden: true
+    //     },
+    //     {
+    //       path: 'productApproverFinance/:id',
+    //       component: () => import('@/views/portal/ServiceManagement/productApproverFinance'),
+    //       name: 'productApproverFinance',
+    //       meta: { title: '特色服务产品审批科技金融', noCache: false }
+
+    //     },
+    //     {
+    //       path: 'findServiceDetail/:id',
+    //       component: () => import('@/views/portal/ServiceManagement/findServiceDetail'),
+    //       name: 'findServiceDetail',
+    //       meta: { title: '特色服务产品查看非科技金融', noCache: false },
+    //       hidden: true
+    //     },
+    //     {
+    //       path: 'findServiceDetailfinance/:id',
+    //       component: () => import('@/views/portal/ServiceManagement/findServiceDetailfinance'),
+    //       name: 'findServiceDetailfinance',
+    //       meta: { title: '特色服务产品查看科技金融', noCache: false },
+    //       hidden: true
+    //     },
+    //     {
+    //       path: 'generalProduct',
+    //       component: () => import('@/views/portal/ServiceManagement/generalProduct'),
+    //       name: 'generalProduct',
+    //       meta: { title: '服务产品管理', noCache: true }
+    //     }
+    //   ]
+    // },
+    // {
+    //   path: 'requireManagement',
+    //   component: () => import('@/views/portal/requireManagement'),
+    //   name: 'requireManagement',
+    //   meta: { title: '需求管理', noCache: true }
+    // },
+    // {
+    //   path: 'requireDetail',
+    //   component: () => import('@/views/portal/requireDetail'),
+    //   name: 'requireDetail',
+    //   meta: { title: '查看详情', noCache: true }
+    // }
   ]
 }
 export default portalRouter

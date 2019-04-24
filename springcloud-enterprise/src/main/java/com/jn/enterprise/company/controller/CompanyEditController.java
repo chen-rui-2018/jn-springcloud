16f7ce52ae036d16bdcc74e304aa63b5ed302e7b
huxw <huxw@op-mobile.com.cn>
2019-04-24 14:16:55 +0800

    1.task：8482 首页-推荐 接口开发和整合 2.task：8479 门户-招商引资 接口整合（TODO） 3.task：8481 首页-资讯 接口开发整合（TODO） 4.task：8478 门户-企业服务首页 接口整合 5.task:8483 门户-首页 重要消息 门户首页弹框接口
    Change-Id: Ia0cd7261ba4e00d9baf911ccabe8692c65c61eb8

---
+++ springcloud-enterprise/src/main/java/com/jn/enterprise/company/controller/CompanyEditController.java
@@ -0,0 +1,34 @@
+package com.jn.enterprise.company.controller;
+
+import com.jn.common.controller.BaseController;
+import com.jn.common.model.Result;
+import com.jn.enterprise.company.model.CompanyUpdateParam;
+import com.jn.system.log.annotation.ControllerLog;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author： huxw
+ * @date： Created on 2019-4-18 16:54:39
+ * @version： v1.0
+ * @modified By:
+ */
+@Api(tags = "企业服务")
+@RestController
+@RequestMapping("/enterprise/company")
+public class CompanyController extends BaseController {
+
+    @ControllerLog(doAction = "编辑企业资料")
+    @ApiOperation(value = "编辑企业资料", notes = "编辑企业资料")
+    @RequestMapping(value = "/updateCompanyInfo",method = RequestMethod.POST)
+    public Result updateCompanyInfo(@Validated @RequestBody CompanyUpdateParam companyUpdateParam){
+        // TODO ibps配置编辑
+        return new Result();
+    }
+
+}

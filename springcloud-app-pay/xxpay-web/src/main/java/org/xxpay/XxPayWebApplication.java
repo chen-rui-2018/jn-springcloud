783828b8fae396c6bcf7a82513077213f10e26c8
chenm <381981766@qq.com>
2019-05-08 10:45:16 +0800

    task:8296 xxpay项目集成，修改web配置文件
    Change-Id: I629e4c2167d24b56fc0707bd5ead825167c59821

--- springcloud-app-pay/xxpay-web/src/main/java/org/xxpay/XxPayWebApplication.java
+++ springcloud-app-pay/xxpay-web/src/main/java/org/xxpay/XxPayWebApplication.java
@@ -1,7 +1,9 @@
 package org.xxpay;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -20,6 +22,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class XxPayWebApplication {
 
 	@Bean

40ea799e7029ba076a12855b2d1fe5e9bd4052d3
yangh <yangh@op-mobile.com.cn>
2019-05-10 12:01:17 +0800

    task8086 园区，企业tab头信息获取，增幅列名称定义，催报接口修复，删除掉多余的接口
    Change-Id: I6eeff93838dca0c658e276c88eee108d1e97b1b3

--- springcloud-enterprise/src/main/java/com/jn/enterprise/data/service/impl/DataUploadServiceImpl.java
+++ springcloud-enterprise/src/main/java/com/jn/enterprise/data/service/impl/DataUploadServiceImpl.java
@@ -34,6 +34,7 @@ import com.jn.news.vo.EmailVo;
 import com.jn.news.vo.SmsTemplateVo;
 import com.jn.system.api.SystemClient;
 import com.jn.system.log.annotation.ServiceLog;
+import com.jn.system.model.SysRole;
 import com.jn.system.model.User;
 import com.jn.system.vo.SysDepartmentPostVO;
 import com.jn.upload.api.UploadClient;
@@ -85,6 +86,9 @@ public class DataUploadServiceImpl implements DataUploadService {
 
     @Autowired(required = false)
     private TbDataReportingGardenFillerMapper tbDataReportingGardenFillerMapper;
+
+    @Autowired(required = false)
+    private TbDataReportingGardenCheckAccessMapper tbDataReportingGardenCheckAccessMapper;
 
     @Autowired
     private DownLoadClient downLoadClient;
@@ -449,22 +453,53 @@ public class DataUploadServiceImpl implements DataUploadService {
         //权限集
         List<GardenFillerAccessModel> access = new ArrayList<>();
         GardenFillerAccessModel gardenFillerAccessModel =null;
-        for(TbDataReportingGardenFiller gardenFiller : taskAccess){
-            //默认没有权限
-            boolean hasAccess = false;
-            for(String departmentId : userAccess){
-                if(gardenFiller.getDepartmentId().equals(departmentId)){
-                    hasAccess = true;
-                    break;
+
+
+
+        List<SysRole> sysRoles =  user.getSysRole();
+        List<String> sysRoleIds = new ArrayList<>();
+        TbDataReportingGardenCheckAccessCriteria accessCriteria = new TbDataReportingGardenCheckAccessCriteria();
+        List<TbDataReportingGardenCheckAccess> dataUploadCheckAccess = null;
+        if(sysRoles !=null && sysRoles.size()>0){
+            for(SysRole sysRole : sysRoles){
+                sysRoleIds.add(sysRole.getId());
+            }
+
+            if(sysRoleIds !=null && sysRoleIds.size()>0){
+                accessCriteria.or().andRecordStatusEqualTo(new Byte(DataUploadConstants.VALID)).andIdIn(sysRoleIds);
+                dataUploadCheckAccess = tbDataReportingGardenCheckAccessMapper.selectByExample(accessCriteria);
+            }
+        }
+
+
+
+        //如果不具备审核权限
+        if(dataUploadCheckAccess==null || dataUploadCheckAccess.size()==0){
+            for(TbDataReportingGardenFiller gardenFiller : taskAccess){
+                //默认没有权限
+                boolean hasAccess = false;
+                for(String departmentId : userAccess){
+                    if(gardenFiller.getDepartmentId().equals(departmentId)){
+                        hasAccess = true;
+                        break;
+                    }
+                }
+                if(hasAccess){
+                    gardenFillerAccessModel = new GardenFillerAccessModel();
+                    BeanUtils.copyProperties(gardenFiller,gardenFillerAccessModel);
+
+                    access.add(gardenFillerAccessModel);
                 }
             }
-            if(hasAccess){
-                gardenFillerAccessModel = new GardenFillerAccessModel();
+        }else{
+            //具备审核权限
+            for(TbDataReportingGardenFiller gardenFiller : taskAccess){
                 BeanUtils.copyProperties(gardenFiller,gardenFillerAccessModel);
-
                 access.add(gardenFillerAccessModel);
             }
         }
+
+
         result.setGardenFiller(access);
 
         return result;

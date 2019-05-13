8d9669fd61cbbdf08da3cd52289a61f995a16d15
wanzy <wangzy@op-mobile.com.cn>
2019-05-13 21:38:39 +0800

    1.统一缴费-添加log日志 2.统一缴费-优化账本查询
    Change-Id: Ie8f695c4adb1f1433c9b9bda70a4883b7d3da598

--- springcloud-enterprise/src/main/java/com/jn/enterprise/pay/controller/MyPayBillController.java
+++ springcloud-enterprise/src/main/java/com/jn/enterprise/pay/controller/MyPayBillController.java
@@ -16,6 +16,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.apache.ibatis.annotations.Param;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,9 +44,10 @@ public class MyPayBillController extends BaseController {
     @Autowired
     private MyPayBillService myPayBillService;
 
-    @ControllerLog(doAction = "我的账单-查询列表")
-    @ApiOperation(value = "我的账单-查询列表",notes = "我的账单-查询列表")
-    @RequestMapping(value = "/billQuery",method = RequestMethod.POST)
+    @ControllerLog(doAction = "我的账单-查询账单信息列表")
+    @ApiOperation(value = "我的账单-查询账单信息列表",notes = "我的账单-查询账单信息列表")
+    @RequestMapping(value = "/billQuery",method = RequestMethod.GET)
+    @RequiresPermissions("/payment/payBill/billQuery")
     public Result<PaginationData<List<PayBillVo>>> billQuery(@Param("payBill")PayBillParams payBill){
         Assert.notNull(payBill.getObjId(),"对象ID不能为空");
         PaginationData<List<PayBillVo>> data = myPayBillService.getBillQueryList(payBill);
@@ -55,6 +57,7 @@ public class MyPayBillController extends BaseController {
     @ControllerLog(doAction = "我的账单-通过账单ID查询账单详情信息")
     @ApiOperation(value = "我的账单-通过账单ID查询账单详情信息",notes = "我的账单-通过账单ID查询账单详情信息")
     @RequestMapping(value = "/getBillInfo",method = RequestMethod.GET)
+    @RequiresPermissions("/payment/payBill/getBillInfo")
     public Result<List<PayBillDetails>> getBillInfo(@ApiParam(name="billId",value = "账单ID或编号",required = true,example = "2019050600025") @RequestParam(value = "billId") String billId){
         Assert.notNull(billId,"账单ID或编号不能为空");
         List<PayBillDetails> data = myPayBillService.getBillInfo(billId);
@@ -65,6 +68,7 @@ public class MyPayBillController extends BaseController {
     @ControllerLog(doAction = "我的账单-核查提醒录入")
     @ApiOperation(value = "我的账单-核查提醒录入",notes = "我的账单-核查提醒录入")
     @RequestMapping(value = "/billCheckReminder",method = RequestMethod.POST)
+    @RequiresPermissions("/payment/payBill/billCheckReminder")
     public Result billCheckReminder(@RequestBody @Validated PayCheckReminder payCheckReminder){
         //获取当前登录用户信息
         User user = (User) SecurityUtils.getSubject().getPrincipal();
@@ -75,6 +79,7 @@ public class MyPayBillController extends BaseController {
     @ControllerLog(doAction = "统一缴费--发起支付")
     @ApiOperation(value = "统一缴费-发起支付")
     @RequestMapping(value = "/startPayment",method = RequestMethod.POST)
+    @RequiresPermissions("/payment/payBill/startPayment")
     public Result startPayment(@RequestBody PayBIllInitiateParam payBIllInitiateParam){
         User user=(User) SecurityUtils.getSubject().getPrincipal();
         return new Result(myPayBillService.startPayment(payBIllInitiateParam,user));

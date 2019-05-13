9ff7dc682a2a23ad50c443e449740cc2c7749fe3
tangry <tangry@op-mobile.com.cn>
2019-05-13 14:17:15 +0800

    task：8800 工程项目管理、工程项目查看的swagger接口开发
    Change-Id: I5f9fd7fcef5943b4d3284ba5c7a2d3520a519688

---
+++ springcloud-park/src/main/java/com/jn/park/plan/model/ProjectProgressRecordModel.java
@@ -0,0 +1,54 @@
+package com.jn.park.plan.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.io.Serializable;
+
+/**
+ * @Author: tangry
+ * @Date: 2019/5/11
+ * @Version 1.0
+ */
+@ApiModel(value = "ProjectProgressRecordModel",description = "工程项目进度记录")
+public class ProjectProgressRecordModel implements Serializable {
+
+
+    private static final long serialVersionUID = 1394564871080192292L;
+
+    @ApiModelProperty(value = "记录时间")
+    private String createdTime;
+
+    @ApiModelProperty(value = "进度")
+    private String progress;
+
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public String getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(String createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public String getProgress() {
+        return progress;
+    }
+
+    public void setProgress(String progress) {
+        this.progress = progress;
+    }
+
+
+    @Override
+    public String toString() {
+        return "ProjectProgressRecordModel{" +
+                "createdTime='" + createdTime + '\'' +
+                ", progress='" + progress + '\'' +
+                '}';
+    }
+}

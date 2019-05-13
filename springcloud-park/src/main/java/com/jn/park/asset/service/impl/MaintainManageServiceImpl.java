20900020d35030ec121419e7ee8b664d3c6170fb
zhuyz@op-mobile.com.cn <zhuyz@op-mobile.com.cn>
2019-05-13 13:01:32 +0800

    task:8363 XQ00008资产管理子系统-房间管理-功能开发 swagger接口编写
    Change-Id: I4f492724eb6202cad960f49e2177934c045492c4

--- springcloud-park/src/main/java/com/jn/park/asset/service/impl/MaintainManageServiceImpl.java
+++ springcloud-park/src/main/java/com/jn/park/asset/service/impl/MaintainManageServiceImpl.java
@@ -1,15 +1,27 @@
 package com.jn.park.asset.service.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.jn.common.model.Page;
+import com.jn.common.model.PaginationData;
 import com.jn.park.asset.dao.MaintainManageDao;
+import com.jn.park.asset.dao.TbAssetMaintainCompanyMapper;
 import com.jn.park.asset.dao.TbAssetMaintainManageMapper;
-import com.jn.park.asset.entity.TbAssetMaintainManage;
-import com.jn.park.asset.entity.TbAssetMaintainManageCriteria;
+import com.jn.park.asset.dao.TbAssetMaintainRecordMapper;
+import com.jn.park.asset.entity.*;
+import com.jn.park.asset.enums.AssetStatusEnums;
+import com.jn.park.asset.model.AssetMaintainCompanyModel;
+import com.jn.park.asset.model.AssetMaintainRecordModel;
 import com.jn.park.asset.model.MaintainManageModel;
 import com.jn.park.asset.service.MaintainManageService;
 import com.jn.system.log.annotation.ServiceLog;
+import com.sun.org.apache.bcel.internal.generic.I2F;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
 * 维保管理impl
@@ -25,32 +37,65 @@ public class MaintainManageServiceImpl implements MaintainManageService {
     private MaintainManageDao maintainManageDao;
     @Autowired
     private TbAssetMaintainManageMapper tbAssetMaintainManageMapper;
+    @Autowired
+    private TbAssetMaintainRecordMapper tbAssetMaintainRecordMapper;
+    @Autowired
+    private TbAssetMaintainCompanyMapper tbAssetMaintainCompanyMapper;
 
     /**
-     * 根据维保信息id获取维保信息
-     * @param id
+     * 根据资产编号id获取维保信息
+     * @param assetNumber
      * @return
      */
     @Override
     @ServiceLog(doAction = "获取维保信息")
-    public MaintainManageModel getMaintain(String id) {
-        MaintainManageModel maintainManageModel = maintainManageDao.getMaintain(id);
+    public MaintainManageModel getMaintain(String assetNumber) {
+        MaintainManageModel maintainManageModel = maintainManageDao.getMaintain(assetNumber);
+        //创建维保厂家model
+        AssetMaintainCompanyModel assetMaintainCompanyModel = new AssetMaintainCompanyModel();
+        //通过厂家id查询对应的厂家
+        TbAssetMaintainCompany tbAssetMaintainCompany = tbAssetMaintainCompanyMapper.selectByPrimaryKey(maintainManageModel.getCompanyId());
+        if (tbAssetMaintainCompany != null){
+            BeanUtils.copyProperties(tbAssetMaintainCompany,assetMaintainCompanyModel);
+            maintainManageModel.setAssetMaintainCompanyModel(assetMaintainCompanyModel);
+        }
+        //维保厂家信息
+        maintainManageModel.setAssetMaintainCompanyModel(assetMaintainCompanyModel);
+        //创建维保记录集合
+        List<AssetMaintainRecordModel> assetMaintainRecordModelList = new ArrayList<>();
+        //通过资产编号查询维保记录
+        TbAssetMaintainRecordCriteria tbAssetMaintainRecordCriteria = new TbAssetMaintainRecordCriteria();
+        TbAssetMaintainRecordCriteria.Criteria criteria = tbAssetMaintainRecordCriteria.createCriteria();
+        criteria.andAssetNumberEqualTo(maintainManageModel.getAssetNumber());
+        criteria.andRecordStatusEqualTo(Byte.parseByte(AssetStatusEnums.EFFECTIVE.getCode()));
+        List<TbAssetMaintainRecord> tbAssetMaintainRecords = tbAssetMaintainRecordMapper.selectByExample(tbAssetMaintainRecordCriteria);
+        if (tbAssetMaintainRecords != null){
+            for (TbAssetMaintainRecord tbAssetMaintainRecord : tbAssetMaintainRecords) {
+                //创建维保记录model
+                AssetMaintainRecordModel assetMaintainRecordModel = new AssetMaintainRecordModel();
+                BeanUtils.copyProperties(tbAssetMaintainRecord,assetMaintainRecordModel);
+                assetMaintainRecordModelList.add(assetMaintainRecordModel);
+            }
+            //维保记录集合
+            maintainManageModel.setAssetMaintainRecordModels(assetMaintainRecordModelList);
+        }
         return maintainManageModel;
     }
 
     /**
-     *  维保信息录入
-     * @param maintainManageModel
+     * 分页获取维保设备列表
+     * @param page
      * @return
      */
     @Override
-    @ServiceLog(doAction = "维保信息录入")
-    public Integer updateMaintain(MaintainManageModel maintainManageModel) {
-        TbAssetMaintainManage tbAssetMaintainManage = new TbAssetMaintainManage();
-        BeanUtils.copyProperties(maintainManageModel,tbAssetMaintainManage);
-        TbAssetMaintainManageCriteria example = new TbAssetMaintainManageCriteria();
-        example.createCriteria().andIdEqualTo(maintainManageModel.getId());
-        int update = tbAssetMaintainManageMapper.updateByExampleSelective(tbAssetMaintainManage, example);
-        return update;
+    @ServiceLog(doAction = "分页获取维保设备列表")
+    public PaginationData<List<MaintainManageModel>> getMaintainList(Page page) {
+        com.github.pagehelper.Page<Object> objects = PageHelper.startPage(page.getPage(), page.getRows());
+        List<MaintainManageModel> maintainManageModelList = maintainManageDao.getMaintainList();
+        PaginationData<List<MaintainManageModel>> data = new PaginationData<>(maintainManageModelList,objects.getTotal());
+        return data;
     }
+
+
+
 }

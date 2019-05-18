d8e0457f45078e83e1b9a966ad23696c575830f8
tangry <tangry@op-mobile.com.cn>
2019-05-18 19:08:09 +0800

    fix:对外提供获取用户报名活动列表
    Change-Id: Ic14c5a549ae16fc72a658a13fc6ecd39e5d06564

--- springcloud-park/src/main/java/com/jn/server/ActivityServerController.java
+++ springcloud-park/src/main/java/com/jn/server/ActivityServerController.java
@@ -1,13 +1,17 @@
 package com.jn.server;
 
 import com.jn.common.controller.BaseController;
+import com.jn.common.model.PaginationData;
 import com.jn.common.model.Result;
+import com.jn.park.activity.model.ActivityApplyListParam;
 import com.jn.park.activity.service.ActivityService;
 import com.jn.park.api.ActivityClient;
 import com.jn.system.log.annotation.ControllerLog;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -32,5 +36,10 @@ public class ActivityServerController extends BaseController implements Activity
     @Override
     public Result<String> getActivityNum(){
         return new Result<>(activityService.getActivityNum());
+    }
+
+    @Override
+    public Result<PaginationData> findActivitySuccessfulRegistration(@RequestBody ActivityApplyListParam activityApplyListParam,@RequestParam("needPage") Boolean needPage) {
+        return new Result<PaginationData>(activityService.findActivitySuccessfulRegistration(activityApplyListParam,needPage));
     }
 }

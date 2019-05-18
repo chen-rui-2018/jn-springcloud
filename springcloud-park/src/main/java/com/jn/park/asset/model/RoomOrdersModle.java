d725149bba3055df4d5ec65c8cd649c20277329b
zhuyz@op-mobile.com.cn <zhuyz@op-mobile.com.cn>
2019-05-18 16:28:35 +0800

    新增房间租借信息,房间租借列表swagger接口
    Change-Id: I6e62d87b3c40639ea7fbf534221f77317fe68228

--- springcloud-park/src/main/java/com/jn/park/asset/model/RoomOrdersModle.java
+++ springcloud-park/src/main/java/com/jn/park/asset/model/RoomOrdersModle.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiModelProperty;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.sql.Date;
+import java.util.List;
 
 /**
 * 房间订单model
@@ -16,16 +17,13 @@ import java.sql.Date;
 public class RoomOrdersModle implements Serializable {
     private static final long serialVersionUID = -6107226237211214310L;
 
-    @ApiModelProperty(value = "主键",example = "")
+    @ApiModelProperty(value = "订单编号",example = "2019051111365096306")
     private String id;
 
-    @ApiModelProperty(value = "租借企业id",example = "")
-    private String enterpriseId;
+    @ApiModelProperty(value = "订单表ID",example = "2019050811515490657")
+    private String orderId;
 
-    @ApiModelProperty(value = "租借企业名称",example = "美的")
-    private String leaseEnterprise;
-
-    @ApiModelProperty(value = "房间id",example = "101")
+    @ApiModelProperty(value = "房间id",example = "575020345132580865")
     private String roomId;
 
     @ApiModelProperty(value = "房间名称",example = "101")
@@ -37,11 +35,41 @@ public class RoomOrdersModle implements Serializable {
     @ApiModelProperty(value = "楼宇id",example = "568817802354360320")
     private String towerId;
 
-    @ApiModelProperty(value = "所属楼宇名称",example = "1号楼")
+    @ApiModelProperty(value = "楼宇名称",example = "1号楼")
     private String towerName;
 
-    @ApiModelProperty(value = "实景图片",example = "实景图片")
-    private String roomUrl;
+    @ApiModelProperty(value = "房间图片")
+    private String imgUrl;
+
+    @ApiModelProperty(value = "面积(平方米)",example = "500")
+    private String roomArea;
+
+    @ApiModelProperty(value = "户型",example = "2室1厅")
+    private String houseType;
+
+    @ApiModelProperty(value = "租金单价(元/平方米/月)",example = "4000")
+    private BigDecimal leasePrice;
+
+    @ApiModelProperty(value = "租金(面积X租金单价) 元/月",example = "20000")
+    private BigDecimal leaseSum;
+
+    @ApiModelProperty(value = "押:press付pay(如:押1付3)",example = "押1付3")
+    private String pressPay;
+
+    @ApiModelProperty(value = "最短租期(月)",example = "6")
+    private String shortestLease;
+
+    @ApiModelProperty(value = "创建时间")
+    private java.sql.Date createTime;
+
+    @ApiModelProperty(value ="介绍",example ="介绍内容")
+    private String introduce;
+
+    @ApiModelProperty(value = "租借企业id")
+    private String enterpriseId;
+
+    @ApiModelProperty(value = "租借企业名称",example = "美的")
+    private String leaseEnterprise;
 
     @ApiModelProperty(value = "开始租借时间",example = "2018-05-01")
     private Date leaseStartTime;
@@ -55,29 +83,14 @@ public class RoomOrdersModle implements Serializable {
     @ApiModelProperty(value = "联系人电话",example = "123456")
     private String contactPhone;
 
-    @ApiModelProperty(value = "面积(平方米)",example = "100")
-    private String roomArea;
+    @ApiModelProperty(value = "房间状态(0空闲,1租借申请中,2租借中,3退租申请中,4已到期)")
+    private Byte roomStatus;
 
-    @ApiModelProperty(value = "租金单价(元/平方米/月)",example = "100")
-    private BigDecimal leasePrice;
+    @ApiModelProperty(value = "创建者账号")
+    private String creatorAccount;
 
-    @ApiModelProperty(value = "介绍",example = "房间介绍")
-    private String introduce;
-
-    @ApiModelProperty(value = "租金(元/月)",example = "10000")
-    private BigDecimal leaseSum;
-
-    @ApiModelProperty(value = "付款(如:押1付3)",example = "押1付3")
-    private String pressPay;
-
-    @ApiModelProperty(value = "最短租期(月)",example = "12")
-    private String shortestLease;
-
-    @ApiModelProperty(value = "合计",example = "40000")
-    private String paySum ;
-
-    @ApiModelProperty(value = "房间状态",example = "0")
-    private String roomStatus;
+    @ApiModelProperty(value = "同属分组房间")
+    private List<RoomOrdersModle> roomOrdersModlesList;
 
     public String getId() {
         return id;
@@ -87,20 +100,12 @@ public class RoomOrdersModle implements Serializable {
         this.id = id;
     }
 
-    public String getEnterpriseId() {
-        return enterpriseId;
+    public String getOrderId() {
+        return orderId;
     }
 
-    public void setEnterpriseId(String enterpriseId) {
-        this.enterpriseId = enterpriseId;
-    }
-
-    public String getLeaseEnterprise() {
-        return leaseEnterprise;
-    }
-
-    public void setLeaseEnterprise(String leaseEnterprise) {
-        this.leaseEnterprise = leaseEnterprise;
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
     }
 
     public String getRoomId() {
@@ -143,12 +148,92 @@ public class RoomOrdersModle implements Serializable {
         this.towerName = towerName;
     }
 
-    public String getRoomUrl() {
-        return roomUrl;
+    public String getImgUrl() {
+        return imgUrl;
     }
 
-    public void setRoomUrl(String roomUrl) {
-        this.roomUrl = roomUrl;
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getRoomArea() {
+        return roomArea;
+    }
+
+    public void setRoomArea(String roomArea) {
+        this.roomArea = roomArea;
+    }
+
+    public String getHouseType() {
+        return houseType;
+    }
+
+    public void setHouseType(String houseType) {
+        this.houseType = houseType;
+    }
+
+    public BigDecimal getLeasePrice() {
+        return leasePrice;
+    }
+
+    public void setLeasePrice(BigDecimal leasePrice) {
+        this.leasePrice = leasePrice;
+    }
+
+    public BigDecimal getLeaseSum() {
+        return leaseSum;
+    }
+
+    public void setLeaseSum(BigDecimal leaseSum) {
+        this.leaseSum = leaseSum;
+    }
+
+    public String getPressPay() {
+        return pressPay;
+    }
+
+    public void setPressPay(String pressPay) {
+        this.pressPay = pressPay;
+    }
+
+    public String getShortestLease() {
+        return shortestLease;
+    }
+
+    public void setShortestLease(String shortestLease) {
+        this.shortestLease = shortestLease;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getIntroduce() {
+        return introduce;
+    }
+
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
+
+    public String getEnterpriseId() {
+        return enterpriseId;
+    }
+
+    public void setEnterpriseId(String enterpriseId) {
+        this.enterpriseId = enterpriseId;
+    }
+
+    public String getLeaseEnterprise() {
+        return leaseEnterprise;
+    }
+
+    public void setLeaseEnterprise(String leaseEnterprise) {
+        this.leaseEnterprise = leaseEnterprise;
     }
 
     public Date getLeaseStartTime() {
@@ -183,67 +268,58 @@ public class RoomOrdersModle implements Serializable {
         this.contactPhone = contactPhone;
     }
 
-    public String getRoomArea() {
-        return roomArea;
-    }
-
-    public void setRoomArea(String roomArea) {
-        this.roomArea = roomArea;
-    }
-
-    public BigDecimal getLeasePrice() {
-        return leasePrice;
-    }
-
-    public void setLeasePrice(BigDecimal leasePrice) {
-        this.leasePrice = leasePrice;
-    }
-
-    public String getIntroduce() {
-        return introduce;
-    }
-
-    public void setIntroduce(String introduce) {
-        this.introduce = introduce;
-    }
-
-    public BigDecimal getLeaseSum() {
-        return leaseSum;
-    }
-
-    public void setLeaseSum(BigDecimal leaseSum) {
-        this.leaseSum = leaseSum;
-    }
-
-    public String getPressPay() {
-        return pressPay;
-    }
-
-    public void setPressPay(String pressPay) {
-        this.pressPay = pressPay;
-    }
-
-    public String getShortestLease() {
-        return shortestLease;
-    }
-
-    public void setShortestLease(String shortestLease) {
-        this.shortestLease = shortestLease;
-    }
-
-    public String getPaySum() {
-        return paySum;
-    }
-
-    public void setPaySum(String paySum) {
-        this.paySum = paySum;
-    }
-
-    public String getRoomStatus() {
+    public Byte getRoomStatus() {
         return roomStatus;
     }
 
-    public void setRoomStatus(String roomStatus) {
+    public void setRoomStatus(Byte roomStatus) {
         this.roomStatus = roomStatus;
+    }
+
+    public String getCreatorAccount() {
+        return creatorAccount;
+    }
+
+    public void setCreatorAccount(String creatorAccount) {
+        this.creatorAccount = creatorAccount;
+    }
+
+    public List<RoomOrdersModle> getRoomOrdersModlesList() {
+        return roomOrdersModlesList;
+    }
+
+    public void setRoomOrdersModlesList(List<RoomOrdersModle> roomOrdersModlesList) {
+        this.roomOrdersModlesList = roomOrdersModlesList;
+    }
+
+    @Override
+    public String toString() {
+        return "RoomOrdersModle{" +
+                "id='" + id + '\'' +
+                ", orderId='" + orderId + '\'' +
+                ", roomId='" + roomId + '\'' +
+                ", roomName='" + roomName + '\'' +
+                ", floor='" + floor + '\'' +
+                ", towerId='" + towerId + '\'' +
+                ", towerName='" + towerName + '\'' +
+                ", imgUrl='" + imgUrl + '\'' +
+                ", roomArea='" + roomArea + '\'' +
+                ", houseType='" + houseType + '\'' +
+                ", leasePrice=" + leasePrice +
+                ", leaseSum=" + leaseSum +
+                ", pressPay='" + pressPay + '\'' +
+                ", shortestLease='" + shortestLease + '\'' +
+                ", createTime=" + createTime +
+                ", introduce='" + introduce + '\'' +
+                ", enterpriseId='" + enterpriseId + '\'' +
+                ", leaseEnterprise='" + leaseEnterprise + '\'' +
+                ", leaseStartTime=" + leaseStartTime +
+                ", leaseEndTime=" + leaseEndTime +
+                ", contactName='" + contactName + '\'' +
+                ", contactPhone='" + contactPhone + '\'' +
+                ", roomStatus=" + roomStatus +
+                ", creatorAccount='" + creatorAccount + '\'' +
+                ", roomOrdersModlesList=" + roomOrdersModlesList +
+                '}';
     }
 }

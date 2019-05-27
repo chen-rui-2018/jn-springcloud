9b6004b5821ea955b57142eab2484071e7b55ed7
tangry <tangry@op-mobile.com.cn>
2019-05-27 16:28:55 +0800

    fix:增加返回结果中一级,二级消息名称,新增查询所有消息接口
    Change-Id: I071fefdef98b539fcf7a7a62d75d7b8c980cff47

--- springcloud-park-api/src/main/java/com/jn/park/message/model/AddMessageModel.java
+++ springcloud-park-api/src/main/java/com/jn/park/message/model/AddMessageModel.java
@@ -34,15 +34,45 @@ public class AddMessageModel implements Serializable {
     @ApiModelProperty("消息发送人")
     private String messageSender;
 
-    @ApiModelProperty("消息一级类别（0：个人动态，1：企业空间）")
-    private Integer messageOneSort;
+    @ApiModelProperty(value = "消息一级分类 （0：个人动态，1：企业空间）")
+    private String messageOneSort;
 
-    @ApiModelProperty("消息二级类别（0：私人订单，1：信用动态，2：园区通知，3：消费汇总，4：收入汇总，5，付款通知，6：企业订单，7：信息发布动态，8：交费提醒，9：访客留言，10：数据上报提醒  11.机构邀请）")
-    private Integer messageTowSort;
+    @ApiModelProperty(value = "消息一级分类名称 （0：个人动态，1：企业空间）")
+    private String messageOneSortName;
+
+    @ApiModelProperty(value = "消息二级分类（0：私人订单，1：信用动态，2：园区通知，3：消费汇总，4：收入汇总，5，付款通知，6：企业订单，7：信息发布动态，8：交费提醒，9：访客留言，10：数据上报提醒  11.机构邀请）")
+    private String messageTowSort;
+
+    @ApiModelProperty(value = "消息二级分类名称（0：私人订单，1：信用动态，2：园区通知，3：消费汇总，4：收入汇总，5，付款通知，6：企业订单，7：信息发布动态，8：交费提醒，9：访客留言，10：数据上报提醒  11.机构邀请）")
+    private String messageTowSortName;
 
     @ApiModelProperty("创建者账号")
     private String creatorAccount;
 
+
+    public void setMessageOneSort(String messageOneSort) {
+        this.messageOneSort = messageOneSort;
+    }
+
+    public String getMessageOneSortName() {
+        return messageOneSortName;
+    }
+
+    public void setMessageOneSortName(String messageOneSortName) {
+        this.messageOneSortName = messageOneSortName;
+    }
+
+    public void setMessageTowSort(String messageTowSort) {
+        this.messageTowSort = messageTowSort;
+    }
+
+    public String getMessageTowSortName() {
+        return messageTowSortName;
+    }
+
+    public void setMessageTowSortName(String messageTowSortName) {
+        this.messageTowSortName = messageTowSortName;
+    }
 
     public String getId() {
         return id;
@@ -100,21 +130,7 @@ public class AddMessageModel implements Serializable {
         this.messageSender = messageSender;
     }
 
-    public Integer getMessageOneSort() {
-        return messageOneSort;
-    }
 
-    public void setMessageOneSort(Integer messageOneSort) {
-        this.messageOneSort = messageOneSort;
-    }
-
-    public Integer getMessageTowSort() {
-        return messageTowSort;
-    }
-
-    public void setMessageTowSort(Integer messageTowSort) {
-        this.messageTowSort = messageTowSort;
-    }
 
     public String getCreatorAccount() {
         return creatorAccount;
@@ -135,8 +151,10 @@ public class AddMessageModel implements Serializable {
                 ", messageConnectName='" + messageConnectName + '\'' +
                 ", messageRecipien='" + messageRecipien + '\'' +
                 ", messageSender='" + messageSender + '\'' +
-                ", messageOneSort=" + messageOneSort +
-                ", messageTowSort=" + messageTowSort +
+                ", messageOneSort='" + messageOneSort + '\'' +
+                ", messageOneSortName='" + messageOneSortName + '\'' +
+                ", messageTowSort='" + messageTowSort + '\'' +
+                ", messageTowSortName='" + messageTowSortName + '\'' +
                 ", creatorAccount='" + creatorAccount + '\'' +
                 '}';
     }

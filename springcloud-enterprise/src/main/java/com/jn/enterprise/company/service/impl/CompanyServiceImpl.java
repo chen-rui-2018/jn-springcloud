bb662132fb087fa86d67b70e8f80231ee8e3f61b
huxw <huxw@op-mobile.com.cn>
2019-05-28 20:48:40 +0800

    修改编辑企业信息接口，入参校验优化
    Change-Id: I593cb7d87f2b35c36f168570c719c726d7db62fc

--- springcloud-enterprise/src/main/java/com/jn/enterprise/company/service/impl/CompanyServiceImpl.java
+++ springcloud-enterprise/src/main/java/com/jn/enterprise/company/service/impl/CompanyServiceImpl.java
@@ -265,15 +265,18 @@ public class CompanyServiceImpl implements CompanyService {
         companyUpdateParam.setCreatedTime(DateUtils.formatDate(new Date(), "yyyy-MM-dd HH:mm:ss"));
         companyUpdateParam.setCreatorAccount(account);
         companyUpdateParam.setRecordStatus(CompanyDataEnum.RECORD_STATUS_VALID.getCode());
+        companyUpdateParam.setComProperty(StringUtils.join(companyUpdateParam.getComPropertyList(), ","));
+        companyUpdateParam.setComPropertyList(null);
 
         // 处理图片
         companyUpdateParam.setAvatar(IBPSFileUtils.uploadFile2Json(account, companyUpdateParam.getAvatar()));
         companyUpdateParam.setBusinessLicense(IBPSFileUtils.uploadFile2Json(account, companyUpdateParam.getBusinessLicense()));
-        List<String> picList = Arrays.asList(companyUpdateParam.getPropagandaPicture().split(","));
+        List<String> picList = Arrays.asList(companyUpdateParam.getPropagandaPictureList());
         String ibpsUrl = IBPSFileUtils.uploadFiles(account, picList);
         if (StringUtils.isNotBlank(ibpsUrl)) {
             companyUpdateParam.setPropagandaPicture(ibpsUrl);
         }
+        companyUpdateParam.setPropagandaPictureList(null);
 
         String bpmnDefId = ibpsDefIdConfig.getUpdateCompanyInfo();
         IBPSResult ibpsResult = IBPSUtils.startWorkFlow(bpmnDefId, account, companyUpdateParam);

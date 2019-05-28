da317850f529f8cc3f5aaa5e3b6654b8d88f3b2f
huangbq <harl2513@qq.com>
2019-05-28 18:19:51 +0800

    增加 关键区域视频监控
    Change-Id: Ia1048342749b8ef4102f2dedc90b30a6624d22a8

--- springcloud-park/src/main/java/com/jn/park/park/controller/ParkAdminController.java
+++ springcloud-park/src/main/java/com/jn/park/park/controller/ParkAdminController.java
@@ -4,64 +4,33 @@ import com.jn.common.controller.BaseController;
 import com.jn.common.model.Result;
 import com.jn.hardware.api.SecurityVideoClient;
 import com.jn.hardware.model.security.SecurityTokenParam;
-import com.jn.park.park.model.ParkGeneral;
-import com.jn.park.park.model.ParkName;
-import com.jn.park.park.service.ParkService;
-import com.jn.park.park.vo.ParkDetailsVo;
 import com.jn.system.log.annotation.ControllerLog;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 
-@Api(tags = "园区管理")
+@Api(tags = "园区管理-后台")
 @RestController
-@RequestMapping(value="/guest/portal/park")
-public class ParkController extends BaseController {
+@RequestMapping(value="/portal/park")
+public class ParkAdminController extends BaseController {
 
-    @Autowired
-    private ParkService parkService;
     @Autowired
     private SecurityVideoClient securityVideoClient;
-
-    @ControllerLog(doAction = "园区管理-获取全部园区数据列表")
-    @ApiOperation(value = "获取全部园区数据列表", notes = "返回全部园区数据列表")
-    @GetMapping(path = "/list")
-    public Result<List<ParkName>> getGardenName()  {
-        List<ParkName> list = parkService.getParkName();
-        return new Result(list);
-    }
-
-    @ControllerLog(doAction = "园区管理-根据ID获取对应园区详情")
-    @ApiOperation(value = "根据ID获取对应园区详情", notes = "返回对应园区数据列表")
-    @GetMapping(path = "/get")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "id", value = "园区ID", dataType = "String", paramType = "query", example = "570327211966464000"),
-    })
-    public Result<ParkDetailsVo> getGardendetails(String id)  {
-        ParkDetailsVo park = parkService.getParkDetails(id);
-        return new Result<>(park);
-    }
-
-    @ControllerLog(doAction = "园区管理-获取园区概况")
-    @ApiOperation(value = "获取园区概况", notes = "返回获取园区概况")
-    @GetMapping(path = "/basic")
-    public Result<ParkGeneral> getGardenGeneral()  {
-        ParkGeneral park=parkService.getParkGeneral();
-        return new Result<>(park);
-    }
 
 
     @ControllerLog(doAction = "打开关键区域视频监控")
     @ApiOperation(value = "打开关键区域视频监控")
     @RequestMapping(value = "/getSecurityTokenURL",method = RequestMethod.GET)
+    @RequiresPermissions(value = "/portal/park/getSecurityTokenURL")
     @ResponseBody
     public void getSecurityTokenURL(HttpServletResponse response, String id){
         SecurityTokenParam securityTokenParam=new SecurityTokenParam();

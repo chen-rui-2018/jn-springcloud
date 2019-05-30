f28d7c32164943f4e876a52434d96815f0ec3a20
wanzy <wangzy@op-mobile.com.cn>
2019-05-30 15:50:24 +0800

    创建账单-修改用户字段
    Change-Id: I26e4571e12686f88814c26216793eb9077911718

--- springcloud-enterprise/src/main/java/com/jn/enterprise/pay/service/impl/MyPayBillServiceImpl.java
+++ springcloud-enterprise/src/main/java/com/jn/enterprise/pay/service/impl/MyPayBillServiceImpl.java
@@ -421,7 +421,11 @@ public class MyPayBillServiceImpl implements MyPayBillService {
                     tpbmr.setNatureCode(BILL_AC_BOOK_TYPE_1.getCode());
                     tpbmr.setMoney(tbs.getBillExpense());
                     tpbmr.setBalance(totalAmount);
-                    tpbmr.setCreatorAccount(user.getAccount());
+                    if (StringUtils.isBlank(user.getAccount())) {
+                        tpbmr.setCreatorAccount(payBillCreateParamVo.getCreatorAccount());
+                    } else {
+                        tpbmr.setCreatorAccount(user.getAccount());
+                    }
                     tpbmr.setCreatedTime(new Date());
                     tpbmr.setRecordStatus(PaymentBillEnum.BILL_STATE_NOT_DELETE.getCode());
                     logger.info("统一缴费插入流水记录入參【{}】", tpbmr.toString());

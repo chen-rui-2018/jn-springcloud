a9c02b3b111722bb5c3baf3b74ac01a8184a784a
yangph <yangph@op-mobile.com.cn>
2019-06-03 21:57:08 +0800

    task:9034 修改报名列表查询逻辑
    Change-Id: I7664acc5dc915f5e716b16d9ce59cb26d6358bb1

--- springcloud-park/src/main/java/com/jn/park/activity/model/ActivityApplyParam.java
+++ springcloud-park/src/main/java/com/jn/park/activity/model/ActivityApplyParam.java
@@ -5,7 +5,9 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.Pattern;
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * 活动申请人查询/导出入参Bean
@@ -21,6 +23,10 @@ public class ActivityApplyParam extends Page implements Serializable {
     @NotBlank(message = "活动id不能为空！")
     private String activityId;
 
+    @ApiModelProperty(value = "活动报名状态(1:报名成功   2:报名待审核，全部传空)",example = "1")
+    @Pattern(regexp = "^[1-2]|(\\s*)$",message = "活动报名状态值只能为 1或 2, 1:报名成功-2报名待审核")
+    private String applyStatus;
+
     @ApiModelProperty(value = "[查询时传空]导出excel导出的字段别名 比如：name,phone,sex ...多个字段以数组传递",example = "['name','phone']")
     private String[] exportColName;
 
@@ -33,6 +39,14 @@ public class ActivityApplyParam extends Page implements Serializable {
 
     public void setActivityId(String activityId) {
         this.activityId = activityId;
+    }
+
+    public String getApplyStatus() {
+        return applyStatus;
+    }
+
+    public void setApplyStatus(String applyStatus) {
+        this.applyStatus = applyStatus;
     }
 
     public String[] getExportColName() {
@@ -49,5 +63,15 @@ public class ActivityApplyParam extends Page implements Serializable {
 
     public void setExportTitle(String[] exportTitle) {
         this.exportTitle = exportTitle;
+    }
+
+    @Override
+    public String toString() {
+        return "ActivityApplyParam{" +
+                "activityId='" + activityId + '\'' +
+                ", applyStatus='" + applyStatus + '\'' +
+                ", exportColName=" + Arrays.toString(exportColName) +
+                ", exportTitle=" + Arrays.toString(exportTitle) +
+                '}';
     }
 }

7249fa9093cd3a6ff3cf8f6d65be08f1f9968fce
hey <heyang@op-mobile.com.cn>
2019-06-04 14:11:12 +0800

    对账结果新增邮件通知
    Change-Id: If6559960eb6999f631ef7edcbb9fc729d691c3b7

--- springcloud-app-pay/xxpay-reconciliation/src/main/java/com/jn/reconciliation/biz/ReconciliationNoticeBiz.java
+++ springcloud-app-pay/xxpay-reconciliation/src/main/java/com/jn/reconciliation/biz/ReconciliationNoticeBiz.java
@@ -16,11 +16,17 @@
 package com.jn.reconciliation.biz;
 
 import com.jn.common.channel.MessageSource;
+import com.jn.common.model.Result;
+import com.jn.common.util.GlobalConstants;
+import com.jn.config.EmailConfig;
+import com.jn.news.vo.EmailVo;
 import com.jn.news.vo.SmsTemplateVo;
 import com.jn.reconciliation.enums.BatchStatusEnum;
 import com.jn.reconciliation.service.PayReconciliationCheckBatchService;
 import com.jn.reconciliation.service.PayReconciliationMistakeService;
 import com.jn.reconciliation.service.ReconciliationNoticeUserService;
+import com.jn.system.api.SystemClient;
+import com.jn.system.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +38,7 @@ import org.xxpay.dal.dao.entity.reconciliation.TbPayReconciliationCheckBatch;
 import org.xxpay.dal.dao.entity.reconciliation.TbPayReconciliationNoticeUser;
 
 import java.math.BigDecimal;
+import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -61,6 +68,11 @@ public class ReconciliationNoticeBiz {
 	@Autowired
 	private PayReconciliationMistakeService payReconciliationMistakeService;
 
+	@Autowired
+	private SystemClient systemClient;
+
+	@Autowired
+	private EmailConfig emailConfig;
 
 
 
@@ -74,24 +86,44 @@ public class ReconciliationNoticeBiz {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		String nowDateStr = sdf.format(new Date());
 
-		/**step 1 获取通知的用户号码  **/
-		//查询出需要通知的用户号码
-		String[] Mobiles;
+
+		/**step 1 获取通知的用户账号  **/
+		List<String> accountList = new ArrayList<>();
 		List<TbPayReconciliationNoticeUser> userList = reconciliationNoticeUserService.getEffectiveUserList();
 		if(null != userList && userList.size() > 0) {
-			Mobiles = new String[userList.size()];
 			for (int i = 0; i < userList.size(); i++) {
-				Mobiles[i] = userList.get(i).getPhone();
+				accountList.add(userList.get(i).getAccount());
 			}
-
-			logger.info("本次对账通知的号码为：{}" , Arrays.toString(Mobiles));
+			logger.info("本次对账通知的账号为：{}" , accountList.toString());
 		}else{
 			logger.info("没有可通知的对象 userList isNull" );
 			logger.info("[对账通知] ====通知结束====");
 			return ;
 		}
 
-		/**step 2 获取短信内容  **/
+		/**step 2 通过账号(account)获取用户信息(手机号码,邮箱) **/
+		//通过接口获取用户信息
+		Result<List<User>> userResult = systemClient.getUserInfoByAccount(accountList);
+		//用户信息集合
+		List<User> userInfoList ;
+		if(GlobalConstants.SUCCESS_CODE.equals(userResult.getCode())){
+			userInfoList = userResult.getData();
+			if(null == userInfoList || userInfoList.size() == 0){
+				logger.info("step 2 通过账号(account)获取用户信息(手机号码,邮箱)   【没有获取到对应的用户信息！！！】");
+				logger.info("[对账通知] ====通知结束====");
+				return ;
+			}
+			logger.info("step 2 通过账号(account)获取用户信息(手机号码,邮箱) 返回参数: {}", userInfoList.toString());
+		}else{
+			logger.info("step 2 通过账号(account)获取用户信息(手机号码,邮箱)  失败!, code ：{},errMsg:{}", userResult.getCode(),userResult.getResult());
+			logger.info("[对账通知] ====通知结束====");
+			return ;
+		}
+
+
+
+
+		/**step 3 计算出对账结果金额  **/
 		// 平台交易总金额
 		BigDecimal platTradeAmount = BigDecimal.ZERO;
 		// 平台实际交易总金额(扣除手续费)
@@ -128,10 +160,63 @@ public class ReconciliationNoticeBiz {
 			}
 		}
 
-		//查询当天差错记录数
+		//查询当天差错记录数（异常订单数）
 		mistakeNum = payReconciliationMistakeService.getMistakeNumByDate(nowDateStr);
 
+
 		/**step 3 发送短信  **/
+
+		try {
+			sendShortMessage(userInfoList, billDateStr, platTradeAmount, platFee, bankAmount, bankActualAmount, bankFee, mistakeNum);
+		} catch (Exception e) {
+			logger.error("[对账短信通知]异常:", e);
+		}
+
+		/**step 4 发送邮件 **/
+		try {
+			sendEmail(userInfoList, billDateStr, platTradeAmount, platFee, bankAmount, bankActualAmount, bankFee, mistakeNum);
+		} catch (Exception e) {
+			logger.error("[对账邮件通知]异常:", e);
+		}
+
+
+		logger.info("[对账通知] ====通知结束====");
+	}
+
+
+
+	/**
+	 * 发送短信通知
+	 * @param userInfoList 用户集合
+	 * @param billDateStr  账单日期
+	 * @param platTradeAmount 平台交易总金额
+	 * @param platFee 平台总手续费
+	 * @param bankAmount 商户交易总金额
+	 * @param bankActualAmount 商户实际收到总金额(扣除手续费)
+	 * @param bankFee 商户总手续费
+	 * @param mistakeNum 差错记录数(异常订单数)
+	* */
+	private void sendShortMessage(List<User> userInfoList
+					,String billDateStr
+					,BigDecimal platTradeAmount
+					,BigDecimal platFee
+					,BigDecimal bankAmount
+					,BigDecimal bankActualAmount
+					,BigDecimal bankFee
+					,Long 		mistakeNum
+	) throws Exception{
+
+		logger.info("[对账短信通知] =======开始=======");
+
+		//获取用户号码
+		String[] Mobiles;
+		Mobiles = new String[userInfoList.size()];
+		for (int i = 0; i < userInfoList.size(); i++) {
+			Mobiles[i] = userInfoList.get(i).getPhone();
+		}
+		logger.info("[对账短信通知] 本次对账短信通知的号码为：{}" , Arrays.toString(Mobiles));
+
+
 		//短信通知
 		SmsTemplateVo smsTemplateVo = new SmsTemplateVo();
 		smsTemplateVo.setTemplateId(MessageNoticesTemplateEnums.RECONCILIATION_TEMPLATE.getCode());
@@ -141,7 +226,63 @@ public class ReconciliationNoticeBiz {
 				,bankActualAmount.toString(),bankFee.toString(),mistakeNum.toString()};
 		smsTemplateVo.setContents(t);
 		messageSource.outputSms().send(MessageBuilder.withPayload(smsTemplateVo).build());
-		logger.info("[对账通知] 通知对账结果,Contents: {}", Arrays.toString(t));
-		logger.info("[对账通知] ====通知结束==== ");
+		logger.info("[对账短信通知] 通知对账结果,Contents: {}", Arrays.toString(t));
+		logger.info("[对账短信通知] =======结束=======");
 	}
+
+
+	/**
+	 * 发送邮件通知
+	 * @param userInfoList 用户集合
+	 * @param billDateStr  账单日期
+	 * @param platTradeAmount 平台交易总金额
+	 * @param platFee 平台总手续费
+	 * @param bankAmount 商户交易总金额
+	 * @param bankActualAmount 商户实际收到总金额(扣除手续费)
+	 * @param bankFee 商户总手续费
+	 * @param mistakeNum 差错记录数(异常订单数)
+	 * */
+	private void sendEmail(List<User> userInfoList
+			,String billDateStr
+			,BigDecimal platTradeAmount
+			,BigDecimal platFee
+			,BigDecimal bankAmount
+			,BigDecimal bankActualAmount
+			,BigDecimal bankFee
+			,Long 		mistakeNum
+	) throws Exception{
+
+		logger.info("[对账邮件通知] =======开始=======");
+		//获取用户邮箱
+		StringBuffer emailBuff = new StringBuffer();
+		for (int i = 0; i < userInfoList.size(); i++) {
+			emailBuff.append(userInfoList.get(i).getEmail()) ;
+			emailBuff.append(",");
+		}
+		//去除最后的逗号
+		emailBuff.deleteCharAt(emailBuff.length() - 1);
+		logger.info("[对账邮件通知]本次通知的邮箱为：{}" , emailBuff.toString());
+
+		//获取短信模板,并把里面的占位符替换
+		String emailContent = MessageFormat.format(emailConfig.getTemplate(), billDateStr,platTradeAmount.toString(),platFee.toString(),bankAmount.toString()
+				,bankActualAmount.toString(),bankFee.toString(),mistakeNum.toString());
+
+		//邮件通知
+		EmailVo emailVo = new EmailVo();
+		emailVo.setEmail(emailBuff.toString());
+		emailVo.setEmailSubject("对账结果通知[账单日期：" + billDateStr +"]");
+		emailVo.setEmailContent(emailContent);
+		messageSource.outputEmail().send(MessageBuilder.withPayload(emailVo).build());
+
+
+		logger.info("[对账邮件通知] 通知对账结果,邮件内容: {}", emailVo.getEmailContent());
+		logger.info("[对账邮件通知] =======结束=======");
+	}
+
+	public static void main(String[] args) {
+		String str1 = "大秦,{0},{0},{0}";
+		System.out.println(MessageFormat.format(str1, "将军", "梦回", "秦朝"));
+
+	}
+
 }

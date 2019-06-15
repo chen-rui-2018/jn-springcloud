03a85cc1c7f69eb11317eefbce579c1e90f5f372
szx <1908409620@qq.com>
2019-06-15 11:08:10 +0800

    数据上报——导出模板的修改
    Change-Id: Ic9221c256d9ed8ee52fd552685796e52babce704

--- springcloud-enterprise/src/main/java/com/jn/enterprise/data/util/POIDescribe.java
+++ springcloud-enterprise/src/main/java/com/jn/enterprise/data/util/POIDescribe.java
@@ -4,6 +4,7 @@ package com.jn.enterprise.data.util;
 import com.jn.enterprise.data.model.CompanyTree;
 import com.jn.enterprise.data.tool.GetCompanyTree;
 import org.apache.commons.lang.time.DateFormatUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -16,18 +17,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 /**
  *
  *  指标描述导出模式
  *
  */
-
 public class POIDescribe {
 
-    public  void getDescribeTable(List<CompanyTree> list, HttpServletRequest req,
-                                        HttpServletResponse resp)throws IOException {
+    public void getDescribeTable(List<CompanyTree> list,HttpServletRequest req,
+                                 HttpServletResponse resp)throws IOException {
 
         String fileName = DateFormatUtils.format(new Date(),"yyyyMMddHHmm") +".xls"; //设置文件名
         resp.setHeader("Content-disposition", "attachment;filename=" + new String(fileName.getBytes("gb2312"), "ISO8859-1"));//设置文件头编码格式
@@ -35,9 +34,8 @@ public class POIDescribe {
         resp.setDateHeader("Expires", 0);//设置日期头
 
         GetCompanyTree getCompanyTree = new GetCompanyTree();
-        List<CompanyTree> treeList = getCompanyTree.bulidscience(list);
-        List<Map> treeList2 = getCompanyTree.bulid(list);
-        List<CompanyTree> companyTreeslist = getCompanyTree.bulidlist(treeList);
+        List<CompanyTree> treeList = getCompanyTree.listToTree(list);
+        List<CompanyTree> companyTreeslist = getCompanyTree.treeToList(treeList);
         //构建HSSFWorkBook
         HSSFWorkbook wb = new HSSFWorkbook();
         // 生成一个样式
@@ -62,13 +60,16 @@ public class POIDescribe {
             headrow.createCell(1).setCellValue(companyTreeslist.get(i).getTargetname());
             //得到目标进展情况、是否已完成、是否已达到序时进度
             String str = companyTreeslist.get(i).getDATA();
-            String[] str2 = str.split(";");
-            int len=str2.length;
-            String progress = str2[0]+";"+str2[1]+";"+str2[2];
-            headrow.createCell(2).setCellValue(progress);
-            for (int j=3;j<len;j++){
-                headrow.createCell(j).setCellValue(str2[j]);
+            if(StringUtils.isNotBlank(str)){
+                String[] str2 = str.split(";");
+                int len=str2.length;
+                String progress = str2[0]+";"+str2[1]+";"+str2[2];
+                headrow.createCell(2).setCellValue(progress);
+                for (int j=3;j<len;j++){
+                    headrow.createCell(j).setCellValue(str2[j]);
+                }
             }
+
         }
         wb.write(resp.getOutputStream());
         resp.getOutputStream().flush();

73dbba01832c062476204f87822848ab3ce4dfe1
hey <heyang@op-mobile.com.cn>
2019-06-20 21:33:47 +0800

    新增钉钉通讯录事件回调（通讯录用户增加、更改、离职 ,部门创建、修改、删除）
    Change-Id: Ibbd0e8222a7905db0433fa3d8ef73d89a4501217

--- springcloud-hardware/src/main/java/com/jn/hardware/config/DingTalkProperties.java
+++ springcloud-hardware/src/main/java/com/jn/hardware/config/DingTalkProperties.java
@@ -24,6 +24,19 @@ public class DingTalkProperties {
      * 钉钉：企业内部应用appsecret
      */
     private String appsecret;
+    /**
+     * 钉钉：企业ID
+     */
+    private String corpId;
+    /**
+     * 钉钉：通讯录事件回调Token
+     */
+    private String addressBookToken;
+    /**
+     * 钉钉：通讯录事件回调Aeskey
+     */
+    private String addressBookAeskey;
+
 
     public String getAppkey() {
         return appkey;
@@ -39,5 +52,29 @@ public class DingTalkProperties {
 
     public void setAppsecret(String appsecret) {
         this.appsecret = appsecret;
+    }
+
+    public String getCorpId() {
+        return corpId;
+    }
+
+    public void setCorpId(String corpId) {
+        this.corpId = corpId;
+    }
+
+    public String getAddressBookToken() {
+        return addressBookToken;
+    }
+
+    public void setAddressBookToken(String addressBookToken) {
+        this.addressBookToken = addressBookToken;
+    }
+
+    public String getAddressBookAeskey() {
+        return addressBookAeskey;
+    }
+
+    public void setAddressBookAeskey(String addressBookAeskey) {
+        this.addressBookAeskey = addressBookAeskey;
     }
 }

ef825ddb8f935beaa5eb639816be7ad9fbebd00e
huangbq <harl2513@qq.com>
2019-06-20 23:07:21 +0800

    增加常年申报的接口
    Change-Id: Ia8f76440dc975ed5c2a0e6d9a5caa41c22bf0273

--- springcloud-enterprise/src/main/java/com/jn/enterprise/pd/evaluate/controller/EvaluateController.java
+++ springcloud-enterprise/src/main/java/com/jn/enterprise/pd/evaluate/controller/EvaluateController.java
@@ -5,11 +5,11 @@ import com.jn.company.api.CompanyClient;
 import com.jn.company.model.ServiceCompany;
 import com.jn.enterprise.pd.evaluate.model.EvaluateCompanyModel;
 import com.jn.enterprise.pd.evaluate.model.EvaluateTargetModel;
+import com.jn.enterprise.pd.evaluate.service.EvaluateService;
 import com.jn.system.log.annotation.ControllerLog;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,17 +30,20 @@ public class EvaluateController {
 
     @Autowired
     private CompanyClient companyClient;
+    @Autowired
+    private EvaluateService evaluateService;
+
 
     @ControllerLog(doAction = "评测结果通知")
     @ApiOperation(value = "评测结果通知", notes = "企业评测完成后，零七优服调用此接口（调用参数：企业代码+评测结果）把评测结果通知到智慧园区，智慧园区再用短信方式通知对应的企业",httpMethod = "POST")
     @PostMapping(value = "/notice")
     @RequiresPermissions("/pd/evaluate/notice")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "companyId",value = "企业代码",example = "1231231",required = true),
-            @ApiImplicitParam(name = "resultCode",value = "评测结果代码（0000：通过，其他：不通过）",example = "0000",required = true),
-            @ApiImplicitParam(name = "resultMsg",value = "评测结果描述",example = "评测通过",required = true)
-    })
-    public Result notice(String companyId,String resultCode,String resultMsg){
+    public Result notice(
+            @ApiParam(name = "companyId", value = "企业ID", required = true) @RequestParam(name = "companyId", required = true) String companyId,
+            @ApiParam(name = "resultCode", value = "评测结果代码（0000：通过，其他：不通过）", required = true) @RequestParam(name = "resultCode", required = true)String resultCode,
+            @ApiParam(name = "projectId", value = "评测项目ID", required = true) @RequestParam(name = "projectId", required = true)String projectId,
+            @ApiParam(name = "projectName", value = "评测项目ID", required = true) @RequestParam(name = "projectName", required = true)String projectName,
+            @ApiParam(name = "resultMsg", value = "评测结果描述", required = true) @RequestParam(name = "resultMsg", required = true)String resultMsg){
         return new Result<>();
     }
 
@@ -48,10 +51,8 @@ public class EvaluateController {
     @ApiOperation(value = "企业基本信息查询", notes = "查询企业的基本信息",httpMethod = "GET")
     @GetMapping(value = "/getCompany")
     @RequiresPermissions("/pd/evaluate/getCompany")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "companyId",value = "企业代码",example = "8afabb60045d40e485e1d4ac04666e8a",required = true)
-    })
-    public Result<EvaluateCompanyModel> getCompany(String companyId){
+    public Result<EvaluateCompanyModel> getCompany(
+            @ApiParam(name = "companyId", value = "企业ID", required = true) @RequestParam(name = "companyId", required = true)String companyId){
         Result<ServiceCompany> result=companyClient.getCompanyDetailByAccountOrCompanyId(companyId);
         EvaluateCompanyModel evaluateCompanyModel=new EvaluateCompanyModel();
         BeanUtils.copyProperties(result.getData(),evaluateCompanyModel);
@@ -63,10 +64,11 @@ public class EvaluateController {
     @ApiOperation(value = "评测指标查询", notes = "根据企业ID返回该企业对应的评测指标内容（列表）",httpMethod = "GET")
     @GetMapping(value = "/getTargetList")
     @RequiresPermissions("/pd/evaluate/getTargetList")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "companyId",value = "企业代码",example = "8afabb60045d40e485e1d4ac04666e8a",required = true)
-    })
-    public Result<List<EvaluateTargetModel>> getTarget(String companyId){
-        return new Result<List<EvaluateTargetModel>>();
+    public Result<List<EvaluateTargetModel>> getTarget(
+            @ApiParam(name = "companyId", value = "企业ID", required = true) @RequestParam(name = "companyId", required = true)String companyId,
+            @ApiParam(name = "dealTimeStart", value = "开始填报账期(月YYYYMM，年YYYY)", required = true) @RequestParam(name = "dealTimeStart", required = true)String dealTimeStart,
+            @ApiParam(name = "dealTimeEnd", value = "结束填报账期(月YYYYMM，年YYYY)", required = true) @RequestParam(name = "dealTimeEnd", required = true)String dealTimeEnd
+            ){
+        return evaluateService.getTarget(companyId,dealTimeStart,dealTimeEnd);
     }
 }

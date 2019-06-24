b37e88aedd0d7085825b88674defae4b35d0c797
huangzf <782227684@qq.com>
2019-06-25 00:07:06 +0800

    cms后台 1 恢复cms全局路由next()
    Change-Id: I6f179145842acf96314a3c9985b731d30db2b5d8

--- html/cms/src/permission.js
+++ html/cms/src/permission.js
@@ -14,7 +14,7 @@ function hasPermission(roles, permissionRoles) {
   if (!permissionRoles) return true
   return roles.some(role => permissionRoles.indexOf(role) >= 0)
 }
-const whiteList = ['/login', '/auth-redirect', '/answerHome']// no redirect whitelist
+// const whiteList = ['/login', '/auth-redirect', '/answerHome']// no redirect whitelist
 
 router.beforeEach((to, from, next) => {
   NProgress.start() // start progress bar
@@ -54,12 +54,12 @@ router.beforeEach((to, from, next) => {
     }
   } else {
     /* has no token*/
-    if (whiteList.indexOf(to.path) !== -1) { // 在免登录白名单，直接进入
-      next()
-    } else {
-      next(`/login?redirect=${to.path}`) // 否则全部重定向到登录页
-      NProgress.done() // if current page is login will not trigger afterEach hook, so manually handle it
-    }
+    // if (whiteList.indexOf(to.path) !== -1) { // 在免登录白名单，直接进入
+    //   next()
+    // } else {
+    //   next(`/login?redirect=${to.path}`) // 否则全部重定向到登录页
+    //   NProgress.done() // if current page is login will not trigger afterEach hook, so manually handle it
+    // }
     next()
   }
 })

5b4ffa3c109cf2efd7da59e19124d75e8cadcac6
申 <>
2019-06-26 20:45:42 +0800

    配置修改
    Change-Id: Ia7bb22133ba819c139f9c1cd6eed159e031ea499

--- html/cms/config/prod.env.js
+++ html/cms/config/prod.env.js
@@ -2,6 +2,6 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"prod"',
   BASE_API: '"https://cms.bxgxq.com/"',
-  BASE_IBPS_LOGOUT_API: '"https://oa.bxgxq.com/ibps/logout.htm"'
+  BASE_IBPS_LOGOUT_API: '"https://oa.bxgxq.com/ibps/logout.htm"',
   IBPS_URL: '"https://oa.bxgxq.com/ibps/d/"'
 }

b18519d0bc6bef680f76fa1428a03198aa172a9e
yuanyy <yuanyy@op-mobile.com.cn>
2019-06-29 12:27:45 +0800

    修改会议考勤接口与会议签到二维码
    Change-Id: I272079dd10e0b769d26fa4d258a5110c7c145ca1

---
+++ springcloud-oa/src/main/java/com/jn/oa/meeting/domain/SystemUrlProperties.java
@@ -0,0 +1,34 @@
+package com.jn.oa.meeting.domain;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+/**
+ * 系统路径配置类
+ *
+ * @author： yuanyy
+ * @date： Created on 2019/6/28 16:48
+ * @version： v1.0
+ * @modified By:
+ **/
+@ConfigurationProperties(
+        prefix = "jn.systemUrl"
+)
+@Component
+@RefreshScope
+public class SystemUrlProperties {
+
+    /**
+     * 系统路径配置
+     */
+    private String systemUrl;
+
+    public String getSystemUrl() {
+        return systemUrl;
+    }
+
+    public void setSystemUrl(String systemUrl) {
+        this.systemUrl = systemUrl;
+    }
+}
--- springcloud-oa/src/main/java/com/jn/oa/meeting/enums/MeetingExceptionEnums.java
+++ springcloud-oa/src/main/java/com/jn/oa/meeting/enums/MeetingExceptionEnums.java
@@ -18,7 +18,10 @@ public enum MeetingExceptionEnums implements CodeAndMsg {
     UPDATE_MEETING_TIME_CONFLICT("6014506","修改失败,会议开始时间不能小于当前时间"),
     MEETING_ATTENDANCE_SIGN_OUT("6014507","签退失败，未签到不能进行签退"),
     MEETING_ATTENDANCE_SIGN_IN("6014508","签到失败，不能进行多次签到"),
-    MEETING_ATTENDANCE_ERROR("6014509","考勤类型异常，不存在该类型")
+    MEETING_ATTENDANCE_ERROR("6014509","考勤类型异常，不存在该类型"),
+    MEETING_ATTENDANCE_CANCELLED_ERROR("6014510","考勤失败，该会议已取消"),
+    MEETING_ATTENDANCE_TO_BEGIN_ERROR("6014511","考勤失败，该会议未开始"),
+    MEETING_ATTENDANCE_COMPLETED_ERROR("6014512","考勤失败，该会议已结束")
     ;
 
     private String code;

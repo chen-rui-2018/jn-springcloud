0c371fc8672ea7b2c6bdb067eadd9860bdd551fe
yangh <yangh@op-mobile.com.cn>
2019-07-01 19:39:42 +0800

    task8835 电表预警接口加上企业名称字段的值
    Change-Id: I93330a6ed3006af984bf638e0fd4d6ef8511298d

--- springcloud-park/src/main/java/com/jn/park/electricmeter/service/impl/MeterRulesServiceImpl.java
+++ springcloud-park/src/main/java/com/jn/park/electricmeter/service/impl/MeterRulesServiceImpl.java
@@ -585,6 +585,7 @@ public class MeterRulesServiceImpl implements MeterRulesService {
                         warningRecord.setRecordStatus(new Byte(MeterConstants.VALID));
                         warningRecord.setCreatedTime(new Date());
                         warningRecord.setMeterCode(book.getMeterCode());
+                        warningRecord.setCompanyName(serviceCompany.getData().getComName());
                         warningRecords.add(warningRecord);
                         msg = msg +",电表号为："+book.getMeterCode();
                         sendSMS(phone, msg);
